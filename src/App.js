e34e72cc7945402e54b683141576019f15cc1b77
Hatemii <hatem.xhezairi96@gmail.com>
2020-04-18 13:44:03 +0200

    handle 3 forms with button -> setState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 
 export default class App extends React.Component {
+  state = {
+    firstexample: true,
+    secondexample: false,
+    thirdexample:false,
+  }
+
   render(){
+
+    const firstExample = (
+      <div>
+        <h2>First Page</h2>
+        <button onClick={()=> this.setState({firstexample: false, secondexample: true})}>Next</button>
+      </div>
+    )
+
+    const secondExample = (
+      <div>
+        <h2>Second Page</h2>
+        <button onClick={()=> this.setState({secondexample: false, thirdexample: true})}>Next</button>
+      </div>
+    )
+
+    const thirdExample = (
+      <div>
+        <h2>Welcome</h2>
+      </div>
+    )
+
+
     return(
       <div>
-      asd
+        {this.state.firstexample ? firstExample : null}
+        {this.state.secondexample ? secondExample : null}
+        {this.state.thirdexample ? thirdExample : null}
       </div>
     )
   }
