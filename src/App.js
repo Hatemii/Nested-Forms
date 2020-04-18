3cc6ba8938423ad71395d14727309cf0a700e84e
Hatemii <hatem.xhezairi96@gmail.com>
2020-04-18 18:39:50 +0200

    Home - ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route , Switch, Link } from "react-router-dom"
+import Home from "./component/Home"
 
 export default class App extends React.Component {
   render(){
     return(
       <div>
+        <BrowserRouter>
+          <Switch>
 
+            <Route path="/" component={Home} />
+
+          </Switch>
+        </BrowserRouter>
       </div>
     )
   }
--- src/component/Home.js
+++ src/component/Home.js
@@ -3,10 +3,32 @@ import { BrowserRouter, Route,Switch } from "react-router-dom"
 
 
 export default class App extends React.Component {
-  render(){
-    return(
-      <div>
 
+  state = {
+    firstexample:true
+  }
+
+  render(){
+
+    const FirstExample = (
+      <div>
+        <h2>Home</h2>
+        <h4>How do you want to recieve your code?</h4>
+          <p>Email <input type="radio"/></p>
+          <p>Phone <input type="radio"/></p>
+
+        <button onClick={() =>
+          this.props.history.replace({
+            pathname:"/second"
+          })
+        }>Next</button>
+
+      </div>
+    )
+
+    return (
+      <div>
+        {this.state.firstexample ? FirstExample : null}
       </div>
     )
   }
